27785d2620ebe09644f9308b57477b3b293a3baf
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-05-19 11:12:13 +0700

    membuat fungsi main(), dan membuat objek  lampuRumah dan memanggil fungsi setSaklarNo untuk ngisi data lampu teras , ruang tamu, kamar tidur , lampu dapur

--- enkapsulapsi.cpp
+++ enkapsulapsi.cpp
@@ -16,3 +16,12 @@ class remoteLampu
             return SaklarNo[i];
         }
 };
+
+int main()
+{
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
